2a216eb7c7a07dfe8dee8696deb31d8cabb435c7
whynot3978 <saverfullier@gmail.com>
2024-03-25 22:46:24 +0300

    Create random story with user options with StoryGenerator()

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,11 @@
 const storyGenerator = require('../src/index');
 
+const customOptions = {
+    characters: ['Michael', 'Sarah'],
+    places: ['cave', 'river'],
+    actions: ['explored', 'discovered'],
+    events: ['hidden cave', 'lost treasure']
+};
+
 const generator = new storyGenerator();
 console.log(generator.generarateRandomStory());
