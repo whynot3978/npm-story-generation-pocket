8d882182b965df55114c5aee69c3ea9dc6402ed4
whynot3978 <saverfullier@gmail.com>
2024-03-25 22:14:56 +0300

    Create class for generating random stories

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const loremIpsum = require('lorem-ipsum');
+const Chance = require('chance');
+const storyData = require('../data/stories.json');
+
+// Класс storyGenerator будет отвечать за генерацию случайных историй на основе данных из объектов в stories.json
+class storyGenerator {
+    constructor() {
+        this.chance = new Chance();
+    }
+
+    generarateRandomStory() {
+        const character = this.chance.pickone(storyData.characters);
+        const place = this.chance.pickone(storyData.places);
+        const action = this.chance.pickone(storyData.actions);
+        const event = this.chance.pickone(storyData.events);
+
+        const story = `${character} ${action} the ${place} and found a ${event}.`;
+        return story;
+    }
+}
+
+module.exports = storyGenerator;
