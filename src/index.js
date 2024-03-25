797026c88b9571fc623ee3440d0172f09f660477
whynot3978 <saverfullier@gmail.com>
2024-03-25 22:28:10 +0300

    Add parametres for story generator options

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ const storyData = require('../data/stories.json');
 
 // Класс storyGenerator будет отвечать за генерацию случайных историй на основе данных из объектов в stories.json
 class storyGenerator {
-    constructor() {
+    constructor(options = {}) {
         this.chance = new Chance();
+        this.options = options;
     }
 
     // Генерирует случайную историю
     generarateRandomStory() {
-        const character = this.chance.pickone(storyData.characters);
-        const place = this.chance.pickone(storyData.places);
-        const action = this.chance.pickone(storyData.actions);
-        const event = this.chance.pickone(storyData.events);
+        const {characters, places, actions, events} = this.options;
+
+        const character = this.chance.pickone(characters || storyData.characters);
+        const place = this.chance.pickone(places || storyData.places);
+        const action = this.chance.pickone(actions || storyData.actions);
+        const event = this.chance.pickone(events || storyData.events);
 
         const story = `${character} ${action} the ${place} and found a ${event}.`;
         return story;
